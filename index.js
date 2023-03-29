40cb69695c38f6a4a0d1be2953c05d8c2932dd79
Luke Zhang <you@example.com>
2023-03-29 14:42:28 -0600

    feat: add get request for DB

--- index.js
+++ index.js
@@ -22,23 +22,66 @@ mongoose.connect(MONGO_URI, {
 // set up user schema
 const userSchema = new mongoose.Schema({
   username: String
-})
+});
 // set up exercise schema
+const exerciseSchema = new mongoose.Schema({
+  description: String,
+  duration: Number,
+  date: String,
+});
 // set up log schema
+const logSchema = new mongoose.Schema({
+  username: String,
+  count: Number,
+  log: [exerciseSchema]
+});
 
 // set up user model
 const User = mongoose.model('User', userSchema);
 
-// post requests for create a new user
-app.post('/api/users', (req, res) => {
+// POST request for create a new user
+app.post('/api/users', async (req, res) => {
   const username = req.body.username;
   const newUser = new User({username});
-  return res.json({ 
-    username: newUser.username,
-    _id: newUser._id
-  })
-  // return response with json object { username: , _id: }
-})
+
+  try {
+    // save new user to DB
+    await newUser.save();
+
+    // return response with json object { username: , _id: }
+    return res.json({ 
+      username: newUser.username,
+      _id: newUser._id
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json("Server error");
+  }
+});
+
+// GET request to get all users in the DB as an array
+app.get('/api/users/', (req, res) => {
+  // get list of users from DB
+  User.find({})
+    .exec()
+    .then((data) => {
+      console.log(data);
+      if (data && data.length) {
+        // return as an array
+        res.json(data);
+      } else {
+        res.send('no user found');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.json({ error: err });
+    });
+});
+  // return list of users from .find() as an array
+
+
+
 
 // // post requests for add exercises
 // app.post('/api/users/:_id/exercises', (req, res) => {
